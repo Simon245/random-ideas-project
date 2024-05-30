109c3e7ee9d91591b49f81c11a1e97f9de65433d
Simon Ball <simonball245@gmail.com>
2024-05-30 17:19:46 +0200

    Get ideas from the api

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,29 +1,9 @@
+import IdeasApi from '../services/ideasApi';
 class IdeaList {
   constructor() {
     this._ideaListEl = document.querySelector('#idea-list');
-    this._ideas = [
-      {
-        id: 1,
-        text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2022-01-02',
-      },
-      {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk is getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2022-01-02',
-      },
-      {
-        id: 3,
-        text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2022-01-02',
-      },
-    ];
+    this._ideas = [];
+    this.getIdeas();
     this._validTags = new Set();
     this._validTags.add('technology');
     this._validTags.add('software');
@@ -31,7 +11,16 @@ class IdeaList {
     this._validTags.add('education');
     this._validTags.add('health');
     this._validTags.add('inventions');
-    this.render();
+  }
+
+  async getIdeas() {
+    try {
+      const res = await IdeasApi.getIdeas();
+      this._ideas = res.data.data;
+      this.render();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   getTagClass(tag) {
